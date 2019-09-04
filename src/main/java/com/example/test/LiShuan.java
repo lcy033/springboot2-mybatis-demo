2b8bfc13736bdf67112b24724920bd8725c2345f
lcy033 <lcy_033@163.com>
2019-09-04 19:30:17 +0800

    add

---
+++ src/main/java/com/example/test/LiShuan.java
@@ -0,0 +1,49 @@
+package com.example.test;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Created by finup on 2019/9/4.
+ */
+public class LiShuan {
+
+    public static String test(TreeNode treeNodes){
+        int l = 0;
+        int r = 0;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (treeNodes != null) {
+            queue.add(treeNodes);
+        }
+
+        while (!queue.isEmpty()){
+            TreeNode treeNode = queue.poll();
+            if (treeNode.getLeft() != null){
+                l = l +1;
+                queue.add(treeNode.getLeft());
+            }
+
+            if (treeNode.getRight() != null){
+                r = r +1;
+                queue.add(treeNode.getRight());
+            }
+
+        }
+
+        if (l > r + 1 || r > l + 1){
+            return "不是"+ l + ":" + r;
+        }
+
+        return "是"+ l + ":" + r;
+    }
+
+    public static void main(String[] args) {
+        TreeNode treeNode = new TreeNode(1);
+        TreeNode treeNode1 = new TreeNode(2);
+        TreeNode treeNode2 = new TreeNode(3);
+        treeNode.setLeft(treeNode1);
+        treeNode.setRight(treeNode2);
+        System.out.println(LiShuan.test(treeNode));
+    }
+}

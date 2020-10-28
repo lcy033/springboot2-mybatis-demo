9252b66dc7f09b0cb2e1b2ce01cfeeb158682efd
lcy033 <lcy_033@163.com>
2020-10-28 15:35:07 +0800

    优化柱子

--- src/main/java/com/example/test/WaterCase.java
+++ src/main/java/com/example/test/WaterCase.java
@@ -19,6 +19,7 @@ public class WaterCase {
                 max = waters[i];
             }
         }
+        System.out.println("左侧：" + max);
         return max;
     }
 
@@ -35,6 +36,7 @@ public class WaterCase {
                 max = waters[i];
             }
         }
+        System.out.println("右侧：" + max);
         return max;
     }
 
@@ -50,17 +52,22 @@ public class WaterCase {
         if (index > nums.length){
             return "超出柱子数量";
         }
-        int num = nums[index];
+        int num = nums[index-1];
+        //如果是第一根或者最后一根
+        if (index == 1 || index == nums.length){
+            return String.format("第%s根柱子存水量是%s", index, 0);
+        }
         //获取左侧跟右侧最高的柱子取最低的一根然后减去当前柱子的高度
         int water = Math.min(getLiftMax(index, nums), getRightMax(index, nums)) - num;
-        return String.format("第%s根柱子存水量是%s", index, water);
+        System.out.println("当前：" + num);
+        return String.format("第%s根柱子存水量是%s", index, Math.max(water, 0));
 
     }
 
     public static void main(String[] args) {
-        int[] nums = {5, 2, 3, 2, 4};
+        int[] nums = {5, 7, 5, 2, 4, 2, 4, 5};
         WaterCase waterCase = new WaterCase();
-        String result = waterCase.getWater(1, nums);
+        String result = waterCase.getWater(8, nums);
         System.out.println(result);
 
     }

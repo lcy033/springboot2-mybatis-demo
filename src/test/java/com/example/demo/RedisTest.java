6272ca04c4fbd8ae55f6f7c4b3ca61e2082bc317
lcy033 <lcy_033@163.com>
2021-01-12 16:23:24 +0800

    新增数据库文档

--- src/test/java/com/example/demo/RedisTest.java
+++ src/test/java/com/example/demo/RedisTest.java
@@ -1,6 +1,14 @@
 package com.example.demo;
 
+import cn.smallbun.screw.core.Configuration;
+import cn.smallbun.screw.core.engine.EngineConfig;
+import cn.smallbun.screw.core.engine.EngineFileType;
+import cn.smallbun.screw.core.engine.EngineTemplateType;
+import cn.smallbun.screw.core.execute.DocumentationExecute;
+import cn.smallbun.screw.core.process.ProcessConfig;
 import com.example.framework.RedisService;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,6 +23,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import redis.clients.jedis.Jedis;
+
+import javax.sql.DataSource;
+import java.util.ArrayList;
 
 /**
  * Created by finup on 2019/9/3.
@@ -100,5 +111,62 @@ public class RedisTest {
 //
 //        lock.unlock();
 //    }
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        //数据源
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        hikariConfig.setJdbcUrl("jdbc:mysql://10.16.12.252:3310/gsvip_role?characterEncoding=UTF-8");
+        hikariConfig.setUsername("aitifen");
+        hikariConfig.setPassword("aitifen");
+        //设置可以获取tables remarks信息
+        hikariConfig.addDataSourceProperty("useInformationSchema", "true");
+        hikariConfig.setMinimumIdle(2);
+        hikariConfig.setMaximumPoolSize(5);
+        DataSource dataSource = new HikariDataSource(hikariConfig);
+        //生成配置
+        EngineConfig engineConfig = EngineConfig.builder()
+                //生成文件路径
+                .fileOutputDir("/Users/finup/Desktop")
+                //打开目录
+                .openOutputDir(true)
+                //文件类型
+                .fileType(EngineFileType.MD).customTemplate("")
+                //生成模板实现
+                .produceType(EngineTemplateType.freemarker).build();
+
+        //忽略表
+        ArrayList<String> ignoreTableName = new ArrayList<>();
+        ignoreTableName.add("test_user");
+        ignoreTableName.add("test_group");
+        //忽略表前缀
+        ArrayList<String> ignorePrefix = new ArrayList<>();
+        ignorePrefix.add("test_");
+        //忽略表后缀
+        ArrayList<String> ignoreSuffix = new ArrayList<>();
+        ignoreSuffix.add("_test");
+        ProcessConfig processConfig = ProcessConfig.builder()
+                //忽略表名
+                .ignoreTableName(ignoreTableName)
+                //忽略表前缀
+                .ignoreTablePrefix(ignorePrefix)
+                //忽略表后缀
+                .ignoreTableSuffix(ignoreSuffix).build();
+        //配置
+        Configuration config = Configuration.builder()
+                //版本
+                .version("1.0.0")
+                //描述
+                .description("数据库设计文档生成")
+                //数据源
+                .dataSource(dataSource)
+                //生成配置
+                .engineConfig(engineConfig)
+                //生成配置
+                .produceConfig(processConfig).build();
+        //执行生成
+        new DocumentationExecute(config).execute();
+    }
 
 }

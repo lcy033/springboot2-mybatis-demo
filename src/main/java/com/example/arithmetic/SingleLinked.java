4a26383b86e1eb1cd0b0c9bb3dfd3b172df25394
lcy033 <lcy_033@163.com>
2021-03-05 15:44:48 +0800

    新增链表

--- src/main/java/com/example/arithmetic/SingleLinked.java
+++ src/main/java/com/example/arithmetic/SingleLinked.java
@@ -1,6 +1,8 @@
 package com.example.arithmetic;
 
+import org.apache.commons.lang.builder.ToStringBuilder;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 
 /**
  * 单链表
@@ -32,7 +34,7 @@ public class SingleLinked {
         return node.idNo;
     }
 
-    public boolean del(String name){
+    public boolean del(String name) {
         SingleLinkedNode node = head;
         while (true) {
             if (node.next != null && StringUtils.equals(node.next.name, name)) {
@@ -46,9 +48,50 @@ public class SingleLinked {
         }
     }
 
-    public void pri(SingleLinkedNode node){
+    /**
+     * 反转链表
+     * @param head
+     * @return
+     */
+    public SingleLinkedNode reverseListNode(SingleLinkedNode head) {
+        if (head.next == null) {
+            return head;
+        }
+        // 上一个节点
+        SingleLinkedNode preNode = null;
+        // 当前节点
+        SingleLinkedNode curNode = head;
+        // 下一个节点
+        SingleLinkedNode nextNode = null;
+        while (curNode != null) {
+            nextNode = curNode.next;
+            curNode.next = preNode;
+            preNode = curNode;
+            curNode = nextNode;
+        }
+        return preNode;
+    }
+
+    /**
+     * 求链表中间节点
+     * @param head
+     * @return
+     */
+    public SingleLinkedNode middleNode(SingleLinkedNode head) {
+        // 快
+        SingleLinkedNode fast = head;
+        // 慢
+        SingleLinkedNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+    public void pri(SingleLinkedNode node) {
         System.out.println(node.name + "-" + node.idNo);
-        if (node.next != null){
+        if (node.next != null) {
             this.pri(node.next);
         }
     }
@@ -58,7 +101,10 @@ public class SingleLinked {
         singleLinked.add("1", "11");
         singleLinked.add("2", "22");
         singleLinked.add("3", "33");
+        singleLinked.add("4", "44");
+        System.out.println("---" + singleLinked.middleNode(singleLinked.head));
         singleLinked.pri(singleLinked.head);
+        singleLinked.pri(singleLinked.reverseListNode(singleLinked.head));
         System.out.println(singleLinked.get("2"));
         System.out.println(singleLinked.del("2"));
         System.out.println(singleLinked.del("4"));
@@ -78,6 +124,14 @@ public class SingleLinked {
         SingleLinkedNode(String name, String idNo) {
             this.name = name;
             this.idNo = idNo;
+        }
+
+        @Override
+        public String toString() {
+            return new ToStringBuilder(this)
+                    .append("name", name)
+                    .append("idNo", idNo)
+                    .toString();
         }
     }
 }
